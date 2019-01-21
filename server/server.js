1864313ff19ba148d4395fdd27874e4ff437951a
Ronil Bhatia <ronilbhatia@gmail.com>
2019-01-20 21:21:34 -0800

    Create separate repo for Todo API

---
+++ server/server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
+
+const mongoose = require('./db/mongoose');
+const Todo = require('./models/todo')
+const User = require('./models/user')
+
+const app = express();
+const port = process.env.PORT || 3000
+
+app.use(bodyParser.json());
+
+// setup POST #create route  
+app.post('/todos', (req, res) => {
+  const todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then(doc => {
+    res.send(doc);
+  }, err => {
+    res.status(400).send(err);
+  })
+});
+
+// setup GET #index route
+app.get('/todos', (req, res) => {
+  Todo.find().then(todos => {
+    res.send({
+      todos,
+      statusCode: 200
+    });
+  }, err => {
+    res.status(400).send(err)
+  })
+})
+
+// setup GET #show route
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then(todo => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({ todo });
+  }, err => {
+    res.status(400).send(err);
+  })
+});
+
+app.listen(port, () => {
+  console.log(`Started on Port ${port}`);
+});
+
+module.exports = app;

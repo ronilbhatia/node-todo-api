8bc4d20e724fdbd3b1739431dc1445a9e8746580
Ronil Bhatia <ronilbhatia@gmail.com>
2019-01-20 21:53:18 -0800

    Add tests for DELETE /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -101,3 +101,41 @@ describe('GET /todo', () => {
       .end(done)
   });
 });
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', done => {
+    const hexId = todos[1]._id.toHexString();
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todo._id).toBe(hexId);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.findById(hexId).then(todo => {
+          expect(todo).toNotExist();
+          done();
+        }).catch(err => done(err));
+      });
+  });
+
+  it('should return 404 if todo is not found', done => {
+    const hexId = new ObjectID().toHexString();
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(404)
+      .end(done)
+  });
+
+  it('should return 404 if object id is not valid', done => {
+    request(app)
+      .get('/todos/123abc')
+      .expect(404)
+      .end(done)
+  });
+});
